61bdaa7d814ac72ca3e3d173d5753f1387c83e26
Tee290605 <yen0948993473@gmail.com>
2026-05-31 00:38:45 +0700

    Gộp nhánh feature-document-list và xử lý xong conflict

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class User {
     constructor(data) {
         Object.assign(this, data);
@@ -19,18 +18,3 @@ class User {
 }
 
 export default User;
-=======
-import pg from "pg";
-import dotenv from "dotenv";
-
-class User {
-    constructor(data) {
-        Object.assign(this, data);
-
-        this.role = this.role || "STUDENT";
-        this.status = this.status || "ACTIVE";
-    }
-}
-
-export default User;
->>>>>>> feature-document-list
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
@@ -8,21 +7,12 @@ import documentRoutes from "./routes/document.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
 import { connectDB } from "../DB/db.js";
 
-=======
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import documentRoutes from "./routes/documentRoutes.js";
-
-dotenv.config();
->>>>>>> feature-document-list
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/documents", documentRoutes);
@@ -38,13 +28,3 @@ const start = async () => {
 };
 
 start();
-=======
-// DOCUMENT API
-app.use("/api/documents", documentRoutes);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
->>>>>>> feature-document-list
