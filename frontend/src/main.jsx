08f2eebf8359133806ee251cc3ae8b7ef88970a3
Tee290605 <yen0948993473@gmail.com>
2026-05-31 11:34:26 +0700

    Resolve merge conflicts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@fontsource/inter";
@@ -10,17 +9,3 @@ createRoot(document.getElementById("root")).render(
     <App />
 
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-console.log("MAIN RUNNING");
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> feature-document-list
